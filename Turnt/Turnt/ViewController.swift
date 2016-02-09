d9ccf459d5272ea4a7a627e7c9a8588e7b937138
Jesse Lurie <jesse@cuttlesoft.com>
2016-02-09 00:43:15 -0500

    stuff

--- Turnt/Turnt/ViewController.swift
+++ Turnt/Turnt/ViewController.swift
@@ -16,15 +16,24 @@ import SwiftyJSON
 
 
 class ViewController: UIViewController, FBSDKLoginButtonDelegate {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-     let loginButton = FBSDKLoginButton()
+        if FBSDKAccessToken.currentAccessToken() != nil{
+            print("logged in")
+            performSegueWithIdentifier("toInitView", sender: self )
+
+            
+        }
+        else{
+        let loginButton = FBSDKLoginButton()
         loginButton.center = self.view.center
         
         loginButton.readPermissions=["user_friends","public_profile","email"]
         self.view.addSubview(loginButton)
         loginButton.delegate = self
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,50 +45,11 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
         //save user data locally, fb_id
         performSegueWithIdentifier("toInitView", sender: self )
-        UserData()
+        let _ = initialLoad()
     }
     
     func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
         print("logged out")
     }
     
-    func UserData(){
-        let meRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
-        meRequest.startWithCompletionHandler{(connection,result,err) -> Void in
-            if err != nil {
-                print(err)
-            }
-            else{
-                print(result)
-                let postsEndpoint: String = "http://songathon.xyz/api/create"
-                let newPost = ["fb_id": result["id"]]
-                NSUserDefaults.standardUserDefaults().setObject(result["id"], forKey: "user_id")
-                NSUserDefaults.standardUserDefaults().synchronize()
-                Alamofire.request(.POST, postsEndpoint, parameters: newPost, encoding: .JSON)
-                    .responseJSON { response in
-                        do {
-                            if let _ = NSString(data:response.data!, encoding: NSUTF8StringEncoding) {
-                                let jsonDictionary = try NSJSONSerialization.JSONObjectWithData(response.data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
-                                let theuser = jsonDictionary["result"] as! String
-                                print(theuser)                            }
-                        } catch {
-                            print("bad things happened")
-                        }
-                }
-            }
-        //save friends fb_ids to find all activitys and save 
-            //array of lat,lon,title,
-        let graphRequest = FBSDKGraphRequest(graphPath: "me/friends", parameters: nil)
-        graphRequest.startWithCompletionHandler{(connection,result,error)-> Void in
-            if error != nil{
-                print(error)
-            }
-            else{
-                NSUserDefaults.standardUserDefaults().setObject(result["data"], forKey: "friends")
-                NSUserDefaults.standardUserDefaults().synchronize()
-
-            }
-        }
-        }
-    }
-}
+   }
